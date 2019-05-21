df296ebc6b0cf19d1d9b18c0de7eafb8d2ed3a99
Connor Hulla <hullaconnor@gmail.com>
2019-05-21 18:46:39 -0500

    Added depth first traversal to find the exit

--- MazeGenerator/src/adjacencymatrix/AdjacencyMatrix.java
+++ MazeGenerator/src/adjacencymatrix/AdjacencyMatrix.java
@@ -3,7 +3,7 @@ package adjacencymatrix;
 
 import java.util.List;
 import java.util.LinkedList;
-
+import java.util.ArrayList;
 public class AdjacencyMatrix {
     private boolean [][] matrix;
     private int size;
@@ -63,6 +63,47 @@ public class AdjacencyMatrix {
          
         }
         return adj;
+    }
+    //trying to find the exit with a depth first search. exit is the last node
+    public ArrayList findPathD(int src, int dest) 
+    {
+        boolean [] visited = new boolean[size];
+        ArrayList<Integer> paths = new ArrayList<>();
+        int curvertex = 0;
+        paths.add(src);
+        
+        if(src == dest)   //if we are at our destination, the path is empty
+            return paths;
+        
+       findPathD(visited, paths, src, dest);
+        
+        return paths;
+    }
+    
+    private void findPathD(boolean [] visited, ArrayList<Integer> paths, int curNode, 
+            int dest)
+    {
+        visited[curNode] = true;
+        
+        if(visited[dest] == true)
+        {
+            return;
+        }
+        for(int i = 0; i < visited.length; i++)
+        {
+            if(matrix[curNode][i] && visited[i] == false)
+            {
+                paths.add(i);
+                findPathD(visited, paths, i, dest);
+                
+                if(visited[dest] == true)
+                    return;
+                
+                paths.remove(new Integer(i));
+            }
+        }
+        
+        visited[curNode] = false;
     }
             
 }
